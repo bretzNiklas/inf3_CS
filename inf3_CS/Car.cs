f62de7a259da3b1f3be0fcece55b3db9c2b0962e
qwertzniki6 <104077966+bretzNiklas@users.noreply.github.com>
2022-12-02 14:20:08 +0100

    Initial commit

---
+++ inf3_CS/Car.cs
@@ -0,0 +1,20 @@
+﻿namespace inf3_CS;
+
+public class Car
+{
+    public Car()
+    {
+        Console.WriteLine("auto erstellt");
+    }
+
+    public void count()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+    
+    
+    
+}
---
+++ inf3_CS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace inf3_CS
+{
+    class test
+    {
+
+        static void Main(string[] args)
+        {
+
+            Car car = new Car();
+            Car car2 = new Car();
+
+            Buffer b = new Buffer();
+            
+            Console.WriteLine(b.empty());
+            b.push(car);
+            b.push(car2);
+            
+
+            
+        }
+    }
+}
